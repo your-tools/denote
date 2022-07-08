49edd0bc2fb4f241be883961394740d1a0724920
Dimitri Merejkowsky <dimitri@dmerej.info>
2022-07-08 17:35:59 +0200

    Start working on the cli

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use time::macros::format_description;
+use time::OffsetDateTime;
+
 fn main() {
-    println!("Hello, world!");
+    let now = OffsetDateTime::now_utc();
+    let format = format_description!("[year][month][day]T[hour]:[minute]:[second]");
+    let formatted_date = now
+        .format(&format)
+        .expect("now should be formatted correctly");
+
+    let template = format!(
+        r#"---
+date: {formatted_date}
+title:
+tags: []
+    "#
+    );
+    println!("{template}")
 }
