f00bc0e9d46a67784e21b59fa20ccef77f8c0fde
Dimitri Merejkowsky <dimitri@dmerej.info>
2022-07-09 19:13:50 +0200

    More bindings

--- src/python.rs
+++ src/python.rs
@@ -1,9 +1,10 @@
+use std::path::PathBuf;
 use std::str::FromStr;
 
 use pyo3::basic::CompareOp;
 use pyo3::exceptions::{PyOSError, PyValueError};
 use pyo3::prelude::*;
-use pyo3::types::{PyDateTime, PyType};
+use pyo3::types::{PyDateTime, PyModule, PyTuple, PyType};
 
 fn unwrap<T>(result: crate::Result<T>) -> PyResult<T> {
     match result {
@@ -17,6 +18,16 @@ fn to_python_result<T>(error: crate::Error) -> PyResult<T> {
         crate::Error::ParseError(e) => Err(PyValueError::new_err(e.to_string())),
         crate::Error::OSError(e) => Err(PyOSError::new_err(e.to_string())),
     }
+}
+
+fn path_buf_to_pathlib(path_buf: PathBuf) -> PyResult<PyObject> {
+    Python::with_gil(|py| {
+        let pathlib = PyModule::import(py, "pathlib")?;
+        let path_class = pathlib.getattr("Path")?;
+        let args = PyTuple::new(py, &[path_buf.to_string_lossy().to_string()]);
+        let res = path_class.call1(args)?;
+        Ok(res.into())
+    })
 }
 
 #[pyfunction]
@@ -117,6 +128,11 @@ impl Metadata {
         self._inner.keywords().to_vec()
     }
 
+    #[getter]
+    fn relative_path(&self) -> String {
+        self._inner.relative_path().to_string_lossy().to_string()
+    }
+
     fn __str__(slf: PyRef<'_, Self>) -> String {
         let metadata = &slf._inner;
         format!("{metadata:?}")
@@ -168,6 +184,101 @@ impl FrontMatter {
     }
 }
 
+#[pyclass]
+struct Note {
+    _inner: crate::Note,
+}
+
+#[pymethods]
+impl Note {
+    #[new]
+    fn new(metadata: &Metadata, text: &str) -> Self {
+        let inner_note = crate::Note {
+            metadata: metadata._inner.clone(),
+            text: text.to_string(),
+        };
+        Note { _inner: inner_note }
+    }
+
+    #[getter]
+    fn relative_path(&self) -> String {
+        self._inner.relative_path().to_string_lossy().to_string()
+    }
+
+    #[getter]
+    fn front_matter(&self) -> FrontMatter {
+        FrontMatter {
+            _inner: self._inner.front_matter().clone(),
+        }
+    }
+
+    pub fn dump(&self) -> String {
+        self._inner.dump()
+    }
+
+    fn __str__(slf: PyRef<'_, Self>) -> String {
+        let inner = &slf._inner;
+        format!("{inner:?}")
+    }
+}
+
+#[pyclass]
+struct NotesRepository {
+    _inner: crate::NotesRepository,
+}
+
+#[pymethods]
+impl NotesRepository {
+    #[classmethod]
+    fn open(_cls: &PyType, base_path: &PyAny) -> PyResult<Self> {
+        // If base_path is a string or a pathlib.Path instance,
+        // as_str will have the correct value.
+        // Otherwise, all bets are off, but let the caller deal with that :)
+        let as_str = base_path.to_string();
+        let path = match PathBuf::from_str(&as_str) {
+            Ok(p) => p,
+            Err(e) => {
+                return Err(PyValueError::new_err(e.to_string()));
+            }
+        };
+        let inner = unwrap(crate::NotesRepository::open(&path))?;
+        Ok(NotesRepository { _inner: inner })
+    }
+
+    #[getter]
+    fn base_path(&self) -> String {
+        self._inner.base_path().to_string_lossy().to_string()
+    }
+
+    fn import_from_markdown(&self, markdown_path: &PyAny) -> PyResult<PyObject> {
+        let as_path = PathBuf::from_str(&markdown_path.to_string())?;
+        let saved_path = unwrap(self._inner.import_from_markdown(&as_path))?;
+        path_buf_to_pathlib(saved_path)
+    }
+
+    fn on_update(&self, relative_path: &PyAny) -> PyResult<PyObject> {
+        let as_path = PathBuf::from_str(&relative_path.to_string())?;
+        let new_path = unwrap(self._inner.on_update(&as_path))?;
+        path_buf_to_pathlib(new_path)
+    }
+
+    fn load(&self, relative_path: &PyAny) -> PyResult<Note> {
+        let as_path = PathBuf::from_str(&relative_path.to_string())?;
+        let note = unwrap(self._inner.load(&as_path))?;
+        Ok(Note { _inner: note })
+    }
+
+    fn save(&self, note: &Note) -> PyResult<PyObject> {
+        let path = unwrap(self._inner.save(&note._inner))?;
+        path_buf_to_pathlib(path)
+    }
+
+    fn __str__(slf: PyRef<'_, Self>) -> String {
+        let inner = &slf._inner;
+        format!("{inner:?}")
+    }
+}
+
 #[pymodule]
 fn denote(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(add, m)?)?;
@@ -175,5 +286,7 @@ fn denote(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
     m.add_class::<Id>()?;
     m.add_class::<Metadata>()?;
     m.add_class::<FrontMatter>()?;
+    m.add_class::<Note>()?;
+    m.add_class::<NotesRepository>()?;
     Ok(())
 }
